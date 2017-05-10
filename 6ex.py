a2db22ea1b7af2ff626cb3cf1b2e59e4f0a82b32
Simon Uribe-Convers <uribe.convers@gmail.com>
2017-05-10 11:21:18 -0500

    Finished chapter 6

--- 6ex.py
+++ 6ex.py
@@ -8,16 +8,21 @@ y = "Those who know %s and those who %s" % (binary, do_not)
 print x
 print y
 
+#prints out a string and a formatted variable
 print "I said: %r." % x
 print "I also said '%s'" % y
 
+#Creates a boolean value
 hilarious = False
+#creates a variable with a string in it and a formatted variable
 joke_evaluation = "Isn't that joke so funny?! %r"
 
+#prints two varaibles
 print joke_evaluation % hilarious
 
 w = "This is the left side of..."
 
 e = "a string with a right side."
 
+#concatenates two varibles
 print w + e
