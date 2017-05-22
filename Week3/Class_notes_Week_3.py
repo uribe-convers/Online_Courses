0fc8299e4003c3b6fe32d92a95ab3f71575d12fb
Simon Uribe-Convers <uribe.convers@gmail.com>
2017-05-22 14:33:28 -0500

    Started week 3 with Boolean values

---
+++ Week3/Class_notes_Week_3.py
@@ -0,0 +1,13 @@
+#Week 3
+
+# Boolean values or TRUE and FALSE values
+
+# They are useful for comparison
+3 < 4
+
+# and the answer is type bool
+
+# Symbols for boolean comparisons are: <, >, >=, <=, == (equal), and != (not equal)
+
+7 == 7.0
+7 != 8
