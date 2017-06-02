19db4eb6050f47fb1ab44862413e216b53fb1352
Simon Uribe-Convers <uribe.convers@gmail.com>
2017-06-02 07:48:05 -0500

    Changed the format of the notes to Markdown with Python syntax highlighting

--- Week4/Class_Notes_Week_4.py
+++ Week4/Class_Notes_Week_4.py
@@ -1,46 +1,62 @@
-# For Loops and Fancy String Manipulation
-# Strings can be compared using operators.
+# For Loops and Fancy String Manipulation  
 
+Strings can be compared using operators.
+
+```python
 animal = "cat"
 animal == "cat"
 animal != "cat"
+```
+They can be compared using Operators and their order in the alphabet letter by letter: >, <, >=, <= are operators that will work
 
-# They can be compared using Operators and their order in the alphabet letter by letter
-# >, <, >=, <=, all work
+```python
 "abracadabra" < "ace"
 "abracadabra" > "ace"
+```
 
-# Capitalization mattres! Capital lettres are less than lower case letters
+Capitalization matters! Capital lettres are less than lower case letters
 
+```python
 "a" <= "A"
+```
 
- # You can always compare diffreent types for equality or inequality
+You can always compare different types for equality or inequality
 
+```python
 "s" != 3
+```
 
- # But you can't order different types
+But you can't order different types
 
+```python
 "s" < 3
+```
 
-# You can see if a str is within another str
+You can see if a str is within another str
 
+```python
 "cad" in "abracadabra"
 
 "c" in "aeiou"
+```
 
-# Order matters
+and of course the order matters
 
+```python
 "zoo" in "ooze"
+```
 
-# Empty str in another word or in itself
+An empty str in another word and in itself
 
+```python
 "" in "abc"
 
 "" in ""
+```
 
-# The function `len()` tells you how long a str is
-# It counts every character including spaces and puctucation!
+The function `len()` tells you how long a str is, and it counts every character including spaces and punctuation!
 
+```python
 len("")
 
 len("abracadabra")
@@ -49,3 +65,6 @@ len("bwa" + "ha" * 10) #remember that order in math matters!
 len("bwa" "ha" * 10)
 
 print("bwa" "ha")
+```
+
+# Indexing and slicing
