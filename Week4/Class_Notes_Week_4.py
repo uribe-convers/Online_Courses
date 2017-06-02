07bdecc6ee62fabd01fb0f5efccb5f33cc6b777a
Simon Uribe-Convers <uribe.convers@gmail.com>
2017-06-02 05:52:20 -0500

    Started Week4, more strings and for loops

---
+++ Week4/Class_Notes_Week_4.py
@@ -0,0 +1,15 @@
+# For Loops and Fancy String Manipulation
+# Strings can be compared using operators.
+
+animal = "cat"
+animal == "cat"
+animal != "cat"
+
+# They can be compared using their order in the alphabet letter by letter
+# >, <, >=, <=, all work
+"abracadabra" < "ace"
+"abracadabra" > "ace"
+
+# Capitalization mattres! Capital lettres are less than lower case letters
+
+"a" <= "A"
