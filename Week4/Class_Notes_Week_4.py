c82de73c8b98716943a439ce68dc59c605632b36
Simon Uribe-Convers <uribe.convers@gmail.com>
2017-06-02 06:19:42 -0500

    Comparing strings with operators and length of of str

--- Week4/Class_Notes_Week_4.py
+++ Week4/Class_Notes_Week_4.py
@@ -5,7 +5,7 @@ animal = "cat"
 animal == "cat"
 animal != "cat"
 
-# They can be compared using their order in the alphabet letter by letter
+# They can be compared using Operators and their order in the alphabet letter by letter
 # >, <, >=, <=, all work
 "abracadabra" < "ace"
 "abracadabra" > "ace"
@@ -13,3 +13,39 @@ animal != "cat"
 # Capitalization mattres! Capital lettres are less than lower case letters
 
 "a" <= "A"
+
+ # You can always compare diffreent types for equality or inequality
+
+"s" != 3
+
+ # But you can't order different types
+
+"s" < 3
+
+# You can see if a str is within another str
+
+"cad" in "abracadabra"
+
+"c" in "aeiou"
+
+# Order matters
+
+"zoo" in "ooze"
+
+# Empty str in another word or in itself
+
+"" in "abc"
+
+"" in ""
+
+# The function `len()` tells you how long a str is
+# It counts every character including spaces and puctucation!
+
+len("")
+
+len("abracadabra")
+
+len("bwa" + "ha" * 10) #remember that order in math matters!
+len("bwa" "ha" * 10)
+
+print("bwa" "ha")
